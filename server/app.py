ef0f4de16cdbc7ff03e3597849233b97107be25f
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-02-21 21:02:34 +0300

    dev: добавлены сессии на стороне сервера

--- server/app.py
+++ server/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+import uuid
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from dotenv import dotenv_values
 from chat import Chat
 
+DEFAULT_PROMPT = 'Отвечай так, как будто бы ты личный виртуальный помощник'
+SESSION_ID = 'sessionId'
+
 config = dotenv_values('.env')
 
 app = Flask(__name__)
+app.secret_key = config.get('SESSION_SECRET_KEY')
 CORS(app)
 
 chat = Chat(
@@ -14,8 +19,17 @@ chat = Chat(
     config.get('YACHAT_GPT_MODEL_URL')
 )
 
+
+def check_session_id():
+    if SESSION_ID not in session:
+        return jsonify({'status': False, 'error': 'Требуется авторизация'})
+
+
 @app.route('/new-msg', methods=['POST'])
 def new_msg():
+    if check_session_id():
+        return check_session_id()
+
     content = request.json.get('content', '')
     model = request.json.get('model', '')
 
@@ -25,18 +39,24 @@ def new_msg():
         'content': res.content
     })
 
+
 @app.route('/prompt', methods=['POST'])
 def set_prompt():
+    if check_session_id():
+        return check_session_id()
+
     prompt = request.json.get('prompt', '')
-
-    prompt = 'Отвечай так, как будто бы ты личный виртуальный помощник' if len(prompt) == 0 else prompt
-
+    prompt = DEFAULT_PROMPT if len(prompt) == 0 else prompt
     chat.setPrompt(prompt)
 
     return jsonify({'status': True})
 
+
 @app.route('/params', methods=['GET'])
 def get_params():
+    if check_session_id():
+        return check_session_id()
+
     prompt = ''
     
     if chat.messages:
@@ -44,6 +64,15 @@ def get_params():
         prompt = first_message.get('content') if first_message.get('type') == 'system' else '' 
 
     return jsonify({'prompt': prompt})
+
+
+@app.route('/ping', methods=['GET'])
+def get_ping():
+    if SESSION_ID not in session:
+        session[SESSION_ID] = uuid.uuid4()
+
+    return jsonify({'status': True})
+
 
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0')
