06c90fbd1f3bfe55295bc28c43878688125d97e8
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-02-25 20:34:21 +0300

    dev: добавлена общая сеть между контейнерами db и server

--- server/app/config.py
+++ server/app/config.py
@@ -2,7 +2,9 @@ from dotenv import dotenv_values
 
 config = dotenv_values('.env')
 
-SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{config.get('POSTGRES_USER')}:{config.get('POSTGRES_PASSWORD')}@localhost:5432/postgres"
+DB_HOST = 'db' if config.get('DEPLOY') == 'true' else 'localhost'
+
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{config.get('POSTGRES_USER')}:{config.get('POSTGRES_PASSWORD')}@{DB_HOST}:5432/postgres"
 
 SESSION_ID = 'sessionId'
 SESSION_SECRET_KEY = config.get('SESSION_SECRET_KEY')
