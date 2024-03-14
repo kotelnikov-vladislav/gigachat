01d060e3e8abb7178c315507de5a3c8639d747a1
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-03-14 20:43:07 +0300

    main: изменена ошибка с пустой историей

--- server/app/chat.py
+++ server/app/chat.py
@@ -5,10 +5,10 @@ from models import Message, db
 from utils import msg_list_model_to_list
 
 class Chat:
-    def __init__(self, gigchat_api_key: str, yaChat_api_key: str, yaChat_model_uri: str):
+    def __init__(self, gigachat_api_key: str, yaChat_api_key: str, yaChat_model_uri: str):
         self.messages = list()
         self.models = {
-            "gigaChat": GigaChat(credentials=gigchat_api_key, verify_ssl_certs=False),
+            "gigaChat": GigaChat(credentials=gigachat_api_key, verify_ssl_certs=False),
             "yaChat": ChatYandexGPT(api_key=yaChat_api_key, model_uri=f'gpt://{yaChat_model_uri}/yandexgpt-lite')
         }
 
@@ -20,7 +20,7 @@ class Chat:
             db.session.delete(message)
         
         db.session.add(systemMessage)
-        db.session.commit();
+        db.session.commit()
 
     def getParams(self, session_id):
         historyMessages = msg_list_model_to_list(
@@ -28,7 +28,10 @@ class Chat:
                 .filter_by(session_id=str(session_id))
                 .filter_by(author='system').all()
         )
-        prompt = historyMessages[0].get('content') if len(historyMessages) > 0 else 'Отвечай так, как будто ты личный ассистент'
+        if len(historyMessages) > 0:
+            prompt = historyMessages[0].get('content') if historyMessages[0].get('content') == '' else 'Отвечай так, как будто ты личный ассистент'
+        else:
+            prompt = 'Отвечай так, как будто ты личный ассистент'
 
         return {
             'prompt': prompt
@@ -44,18 +47,18 @@ class Chat:
         historyMessagesForBot = list()
 
         for message in historyMessages:
-            if message.get('author') == 'system':
-                content= message.get('content') if len(message.get('content')) > 0 else 'Отвечай так, как будто ты личный ассистент'
+            if message.get('author') is 'system':
+                content= message.get('content') if message.get('content') == '' else 'Отвечай так, как будто ты личный ассистент'
                 historyMessagesForBot.append(SystemMessage(content=content))
-            elif message.get('author') == 'ai':
+            elif message.get('author') is 'ai':
                 historyMessagesForBot.append(AIMessage(content=message.get('content')))
             else:
                 historyMessagesForBot.append(HumanMessage(content=message.get('content')))
 
-        historyMessagesForBot.append(HumanMessage(content=content))
-
         if len(historyMessagesForBot) == 0:
             historyMessagesForBot = [SystemMessage(content='Отвечай так, как будто ты личный ассистент')]
+
+        historyMessagesForBot.append(HumanMessage(content=content))
 
         bot_answer = self.models[model](historyMessagesForBot)
 
@@ -67,4 +70,3 @@ class Chat:
         db.session.commit()
         
         return bot_answer
-
